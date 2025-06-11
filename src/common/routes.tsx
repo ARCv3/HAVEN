fa319e7c235325e04ea7bbf9eff7849151e048b9
israel.aristide <israel.aristide@plusgrade.com>
2025-06-11 13:06:41 -0400

    move routes and main

--- src/common/routes.tsx
+++
@@ -1,7 +0,0 @@
-import { type RouteObject } from "react-router";
-
-const routes : RouteObject[] = [
-
-]
-
-export default routes;
--- src/haven/main.tsx
+++ src/haven/main.tsx
@@ -1,8 +1,8 @@
 import { createRoot } from 'react-dom/client'
-import App from './common/App.tsx'
+import App from '../common/App.tsx'
 
 import { ApolloProvider } from '@apollo/client';
-import graphqlClientConfig from './common/config/graphqqlClientConfig.ts';
+import graphqlClientConfig from '../common/config/graphqqlClientConfig.ts';
 
 createRoot(document.getElementById('root')!).render(
   <ApolloProvider client={graphqlClientConfig}>
